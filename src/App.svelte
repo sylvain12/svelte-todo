c817c74777b416e91d1793679f353eb594d5f931
Sylvain Kadjo <sylvainka12@gmail.com>
2020-01-26 01:17:26 +0000

    Implement Show todos by completed or active or all

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,14 @@
   import Todo from './Todo.svelte';
   import NavBar from './NavBar.svelte';
   import AddTodo from './AddTodo.svelte';
+  import Button from './Button.svelte';
+  import {beforeUpdate, onMount} from 'svelte';
 
   let title;
+  let checkAll = false;
+  $: showedTodo = 'all';
+  $: filterShowTodo = []
+  $: countActiveTodos = 0;
 
   let todos = [
     {
@@ -19,22 +25,7 @@
     {
       id:2,
       title: "Visit my brother",
-      completed: true,
-    },
-    {
-      id:3,
-      title: "Go to sport",
       completed: false,
-    },
-    {
-      id:4,
-      title: "Improve my svelte app",
-      completed: false,
-    },
-    {
-      id:5,
-      title: "Deploy svelte app to zeit",
-      completed: true,
     },
   ];
 
@@ -68,9 +59,80 @@
       return todo
     })
   }
+
+  const handleClearTodos = () => {
+    todos = todos.filter(todo => !todo.completed);
+  }
+  /*
+  const handleCheckUncheckAll = () => {
+    if(checkAll == true) {
+    todos = todos.map(todo => {
+      todo.completed = false;
+      return todo;
+    });
+    }else if(checkAll == false) {
+    todos = todos.map(todo => {
+      todo.completed = true;
+      return todo;
+    });
+    }
+   checkAll = !checkAll;
+  }
+  */
+
+  /* Funcions for lifecycle*/
+  beforeUpdate(() => {
+    if(showedTodo == 'active') {
+      filterShowTodo = todos.filter(todo => !todo.completed)
+    }else if(showedTodo == 'completed') {
+      filterShowTodo = todos.filter(todo => todo.completed)
+    }else {
+      filterShowTodo = todos;
+    }
+
+    countActiveTodos = todos.filter(todo => !todo.completed).length;
+  })
 </script>
 
 <style>
+
+  .button__filter-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .button__filter-box input[type="radio"] {
+    display: none;
+  }
+
+  .button__filter-box label {
+    padding: .5rem .8rem;
+    border: 1px solid rgb(73, 73, 180);
+    cursor: pointer;
+    color: rgb(73, 73, 180);
+  }
+
+  .button__filter-box label:nth-of-type(2) {
+    border-right: none;
+    border-left: none;
+  }
+
+  .button__filter-box input[type="radio"]:checked  + label {
+    background: rgb(73, 73, 180);
+    color: #fff;
+  } 
+
+  .todo-info {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .todo-info p {
+    justify-self: flex-end;
+  }
+
   @media (min-width: 640px) {
   }
 </style>
@@ -85,19 +147,48 @@
     <AddTodo bind:addTodoTitle={title} on:addTodo={handleAddTodo}/>
   </section>
   
-  <section>
-    <hr>
-  </section>
 
   <section>
-    {#if todos.length === 0}
+    {#if filterShowTodo.length === 0}
       <p>No todos</p>
     {:else}
-      {#each todos as todo (todo.id)}
+      {#each filterShowTodo as todo (todo.id)}
         <Todo todo="{todo}" on:changeTodoStatus={handleTodoState} on:removeTodo={handleRemoveTodo}/>
       {/each}
     {/if}
 
+  </section>
+
+  <section>  
+  <hr>
+    <div class="todo-info">
+      <!-- <label for="todos-check-all">
+        <input on:click="{handleCheckUncheckAll}" type="checkbox" bind:value="{checkAll}" id="todos-check-all"> {#if !checkAll}Check{:else}Uncheck{/if} All
+      </label> -->
+
+      <p>{countActiveTodos} {#if countActiveTodos == 1}todo{:else}todos{/if} left</p>
+    </div>
+
+ <hr>
+  </section>
+ 
+  
+  <section class="section-button-action">
+    <div class="ddd">
+      <div class="button__filter-box">
+        
+        <input bind:group="{showedTodo}" type="radio" value="all" id="todo-all"> 
+        <label for="todo-all">All</label>
+
+        <input bind:group="{showedTodo}" type="radio" value="completed" id="todo-completed">
+       <label for="todo-completed">Completed</label>
+  
+        <input id="todo-active" bind:group="{showedTodo}" type="radio" value="active">
+        <label for="todo-active">Active</label>
+     
+      </div> 
+    </div>
+    <Button on:click={handleClearTodos}>Clear completed</Button>
   </section>
    
 </div>
